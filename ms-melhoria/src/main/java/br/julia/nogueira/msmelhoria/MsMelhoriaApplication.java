574944347142c49a6628eeec47ddc8b14b72f7d1
Julia <julia.nunesng@gmail.com>
2023-10-17 19:45:53 -0300

    feat-01/ms-melhoria

---
+++ ms-melhoria/src/main/java/br/julia/nogueira/msmelhoria/MsMelhoriaApplication.java
@@ -0,0 +1,13 @@
+package br.julia.nogueira.msmelhoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsMelhoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsMelhoriaApplication.class, args);
+	}
+
+}
---
+++ ms-melhoria/src/test/java/br/julia/nogueira/msmelhoria/MsMelhoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.julia.nogueira.msmelhoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsMelhoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
